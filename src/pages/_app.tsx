4f289adec0afd441dd4618bd5dcf1d37221041fa
6notes <jlu6.proton@proton.me>
2023-04-09 23:21:46 -0700

    Add google tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const googleTagID = process.env.GOOGLE_TAG_ID;
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleTagID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${googleTagID}');
+        `}
+      </Script>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
